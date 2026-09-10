31392a15cebb29489e2abc8824a9e2dec6171bba
k-miura-neos0909 <k-miura@ne-os.jp>
2026-09-10 16:48:42 +0900

    Fix trailing space in bucket name

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'region' => 'ap-northeast-1',
-    'bucket' => 'miura-test-bucket-20260909 ',
+    'bucket' => 'miura-test-bucket-20260909',
     'url_expiration' => '+15 minutes',
     'max_file_size' => 5 * 1024 * 1024,
 ];
